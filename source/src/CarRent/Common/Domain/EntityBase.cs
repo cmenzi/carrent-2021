66e46111a2d37a00313afdf1c6fb5d3682ae31d9
Cédric Menzi <cedric.menzi@buhlergroup.com>
2021-01-05 20:16:00 +0100

    entitybase added

---
+++ source/src/CarRent/Common/Domain/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRent.Common.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public abstract class EntityBase
+    {
+        protected EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; }
+    }
+}
