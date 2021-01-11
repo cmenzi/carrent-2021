67f2c73d35612157be6b024051415a0bd1f7455a
Cédric Menzi <cedric.menzi@buhlergroup.com>
2021-01-11 17:53:36 +0100

    add repository methods

--- source/src/CarRent/CustomerManagement/Domain/ICustomerRepository.cs
+++ source/src/CarRent/CustomerManagement/Domain/ICustomerRepository.cs
@@ -7,5 +7,14 @@
 
     public interface ICustomerRepository
     {
+        Customer FindById(int id);
+
+        IEnumerable<Customer> FindByName(string name);
+
+        void Add(Customer customer);
+
+        void Remove(Customer customer);
+
+        void Remove(int id);
     }
 }
---
+++ source/src/CarRent/CustomerManagement/Infrastructure/InMemoryCustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRent.CustomerManagement.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class InMemoryCustomerRepository
+    {
+    }
+}
